360f53ff144e9eea8b44683affebac07d6e53ff2
rnldvrgl <delacruz.ronaldvergel@gmail.com>
2025-01-30 15:56:21 +0800

    chore: add color pallete

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,30 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        charcoal: 'var(--charcoal)',
+        offwhite: 'var(--offwhite)',
+        coral: 'var(--coral)',
+        blue: 'var(--blue)',
+        nude: 'var(--nude)',
+        red: 'var(--red)',
+        pinkdust: 'var(--pinkdust)',
+        white: 'var(--white)',
+        black: 'var(--black)',
+        newRed: 'var(--new-red)',
+        pink: 'var(--pink)',
+        newPink: 'var(--new-pink)',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
